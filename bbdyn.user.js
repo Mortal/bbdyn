05cf3125e91dd90cec37bacf81362fb25de6b1e8
Mathias Rav <rav@cs.au.dk>
2016-04-20 10:27:08 +0200

    Bump version

--- bbdyn.user.js
+++ bbdyn.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        BlackBoard improvements
-// @namespace   http://users-cs.au.dk/rav/
+// @namespace   http://users-cs.au.dk/rav/bbdyn-dev/
 // @description Adds live filtering and search form redisplay
 // @include     https://bb.au.dk/*
-// @version     0.7
+// @version     0.7pre1
 // @grant       GM_xmlhttpRequest
 // @grant       GM_log
 // @updateURL   https://github.com/Mortal/bbdyn/raw/stable/bbdyn.user.js
